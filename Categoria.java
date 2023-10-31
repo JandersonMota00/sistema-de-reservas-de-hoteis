caffe85c83d0549fb53c8c041cf2f533feb1fcfc
JandersonMota00 <143976116+JandersonMota00@users.noreply.github.com>
2023-10-31 12:09:00 -0300

    Add files via upload

---
+++ Categoria.java
@@ -0,0 +1,27 @@
+import java.util.Objects;
+
+public class Categoria {
+    private String nomeDaCategoria;
+    private Double preco;
+
+    public Categoria(String nomeDaCategoria, Double preco) {
+        this.nomeDaCategoria = nomeDaCategoria;
+        this.preco = preco;
+    }
+    public String getnomeDaCategoria() {
+        return nomeDaCategoria;
+    }
+    public Double getPreco() {
+        return preco;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null  || getClass() != obj.getClass())
+            return false;
+        Categoria nomeDaCategoria = (Categoria) obj;
+        return Objects.equals(nomeDaCategoria, Categoria.nomeDaCategoria);
+    }
+}
