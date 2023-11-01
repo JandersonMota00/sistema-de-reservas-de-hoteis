66c16fa9f38ea1bcb9d671e42606ed046f0762f7
JandersonMota00 <143976116+JandersonMota00@users.noreply.github.com>
2023-11-01 16:43:36 -0300

    Add files via upload
    A objeto da classe 'ListaDeCategoria' conversa com o objeto da classe 'Categoria' e faz uma pesquisa se um determinado objeto existe. Caso exista, será adicionado no vetor.

---
+++ ListaDeCategoria.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class ListaDeCategoria {
+    private ArrayList<Categoria> lista;
+
+    public ListaDeCategoria() { //construtor
+        this.lista = new ArrayList<>();
+    }
+
+    public ArrayList<Categoria> getLista() {
+        return lista;
+    }
+
+    public boolean addCategoriaNaLista(Categoria categoria) {
+        if (pesquisarCategoria(categoria)){
+            getLista().add(categoria);
+            return true;
+        } 
+        return false;
+    }
+    private boolean pesquisarCategoria (Categoria categoria) {
+        for (int i = 0; i < getLista().size(); i++) {
+            if(getLista().get(i).getnomeDaCategoria().equals(categoria.getnomeDaCategoria())){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Já existe!";
+    }
+}
