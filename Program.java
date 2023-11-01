726aa0ea5bcebb733e5cdb575e57f9e7b07c601d
JandersonMota00 <143976116+JandersonMota00@users.noreply.github.com>
2023-11-01 18:36:20 -0300

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,5 +1,17 @@
 public class Program {
     public static void main(String[] args) {
+        ListaDeCategoria ltc1 = new ListaDeCategoria();
+        ltc1.addCategoriaNaLista(new Categoria("Luxo", 1000.0));
+        System.out.println(ltc1);
+
+        ListaDeCategoria ltc2 = new ListaDeCategoria();
+        ltc1.addCategoriaNaLista(new Categoria("Basico", 150.0));
+        System.out.println(ltc2);
+        
+
+
+
+        /*
         ListaDeCategoria minhaCategoria = new ListaDeCategoria();
         Categoria c1 = new Categoria("Saseqwdq", 3.00);
         minhaCategoria.addCategoriaNaLista(c1);
@@ -18,5 +30,6 @@ public class Program {
         }
         minhaCategoria.addCategoriaNaLista(new Categoria("Basico", 2.00));
         minhaCategoria.addCategoriaNaLista(new Categoria("Luxo", 1.50));
+        */
     }
 }
