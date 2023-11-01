8bf4a9c671b6970f22b12be6f8826c03210a39d3
JandersonMota00 <143976116+JandersonMota00@users.noreply.github.com>
2023-11-01 16:34:04 -0300

    Update Categoria.java
    Tratamento do método equals.

--- Categoria.java
+++ Categoria.java
@@ -1,9 +1,7 @@
-import java.util.Objects;
-
 public class Categoria {
     private String nomeDaCategoria;
     private Double preco;
-
+    
     public Categoria(String nomeDaCategoria, Double preco) {
         this.nomeDaCategoria = nomeDaCategoria;
         this.preco = preco;
@@ -11,17 +9,28 @@ public class Categoria {
     public String getnomeDaCategoria() {
         return nomeDaCategoria;
     }
+    public void setNomeDaCategoria(String nomeDaCategoria) {
+        this.nomeDaCategoria = nomeDaCategoria;
+    }
     public Double getPreco() {
         return preco;
+    }
+    public void setPreco(Double preco) {
+        this.preco = preco;
     }
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null  || getClass() != obj.getClass())
+        if (this == obj){
+            if (obj instanceof Categoria) {
+                // transforma OBJ da classe Object, na classe Categoria.
+                Categoria categoriaAuciliar = (Categoria) obj; //typecast
+                return this.getnomeDaCategoria().equals(categoriaAuciliar.getnomeDaCategoria());
+            }
+        }
+        if (obj == null  || getClass() != obj.getClass()) {
             return false;
-        Categoria categoria = (Categoria) obj;
-        return nomeDaCategoria.equals(categoria.nomeDaCategoria);
+        }
+        return false;
     }
 }
