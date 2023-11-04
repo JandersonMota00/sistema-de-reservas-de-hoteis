502f8630e2de4354387626bec3acf7f1c316022e
JandersonMota00 <143976116+JandersonMota00@users.noreply.github.com>
2023-11-04 19:56:32 -0300

    Update Hotel.java

--- Hotel.java
+++ Hotel.java
@@ -1,3 +1,19 @@
+import java.util.ArrayList;
+
 public class Hotel {
+    private String nome;
+    private ArrayList<Quarto> quartos;
     
+    public String getNome() {
+      return nome;
+    }
+    public void setNome(String nome) {
+      this.nome = nome;
+    }
+    public ArrayList<Quarto> getQuartos() {
+      return quartos;
+    }
+    public void setQuartos(ArrayList<Quarto> quartos) {
+      this.quartos = quartos;
+    }
 }
